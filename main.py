7f8d5a57e81ba558ce85262085df27809dfd2d49
Arjun Gandhi <contact@arjungandhi.com>
2025-11-27 15:40:34 -0700

    wip code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import cadquery as cq
 
-from gridfinity import baseplate
+from gridfinity import baseplate, bin
 
-output = baseplate(2, 3)
-
+output = bin(1, 1, 1)
 # Export to STL file (for 3D printing)
 cq.exporters.export(output, "outputs/output.stl")
